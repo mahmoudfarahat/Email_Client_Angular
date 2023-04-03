f7ac2b4853285ddbfb61b3f1cda6282f7fb52ae3
Mahmoud Farahat <mahmoud.ped@gmail.com>
2023-04-03 23:45:47 +0200

    1.2

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -6,24 +6,33 @@ interface UsernameAvailavbleResponse
   available:boolean
 }
 
+interface signupcredentials{
+username:string
+password:string
+passwordConfirmation:string
+}
+interface signupResponse{
+
+}
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+rootUrl ='https://api.angular-email.com'
 
   constructor(private http:HttpClient) { }
 
 
 
   usernameAvailable(username:string){
-    return this.http.post<UsernameAvailavbleResponse>('https://api.angular-email.com/auth/username',{username})
+    return this.http.post<UsernameAvailavbleResponse>(this.rootUrl+'/auth/username',{username})
   }
 
 
-  signup(credentials :any){
-    return this.http.post<any>('https://api.angular-email.com/auth/signup',credentials)
+  signup(credentials :signupcredentials){
+    return this.http.post<signupResponse>(`${this.rootUrl}/auth/signup`,credentials)
   }
 
 
--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -14,24 +14,27 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
+  authForm :FormGroup | any
+  constructor(private matchPassword:MatchPassword , private asyncUnique:AsyncUnique  , private authservice:AuthService) {
 
-authForm = new FormGroup({
-  username:new FormControl('',
-   [Validators.required ,Validators.minLength(3),Validators.maxLength(20), Validators.pattern(/^[a-z0-9]+$/)],
-    [this.asyncUnique.validate]
-   ),
-password:new FormControl('' ,[Validators.required , Validators.minLength(4),Validators.maxLength(20)]),
-passwordConfirmation: new FormControl('', [Validators.required ,Validators.minLength(4),Validators.maxLength(20)])
-},
-// MatchPassword
- {validators:[this.matchPassword.validate]}
+   }
 
- )
-  constructor(private matchPassword:MatchPassword , private asyncUnique:AsyncUnique  , private authservice:AuthService) { }
+
 
   ngOnInit(): void {
+      this.authForm = new FormGroup({
+      username:new FormControl('', [Validators.required ,Validators.minLength(3),Validators.maxLength(20), Validators.pattern(/^[a-z0-9]+$/)],
+          [this.asyncUnique.validate]
+         ),
+      password:new FormControl('' ,[Validators.required , Validators.minLength(4),Validators.maxLength(20)]),
+      passwordConfirmation: new FormControl('', [Validators.required ,Validators.minLength(4),Validators.maxLength(20)])
+      },
+      // MatchPassword
+       {validators:[this.matchPassword.validate]}
+       )
   }
   onSubmit(){
+
     if(this.authForm.invalid){
       return;
     }
